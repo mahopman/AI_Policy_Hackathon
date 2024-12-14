4e1f303033bbfbc6b5a57a4c83d8a0ca03df8463
JackWittmayer <jwittmayer@ufl.edu>
2024-12-14 10:29:06 -0800

    Added attribute.py

---
+++ attribute.py
@@ -0,0 +1,17 @@
+class Attribute:
+
+    def __init__(self, name, description, possible_values):
+        self.name = name
+        self.description = description
+        self.possible_values = possible_values
+        values_text = f" Possible values: {', '.join(self.possible_values)}" if self.possible_values else ""
+        self.observer_prompt = f"{self.name}: [{self.description}{values_text}]"
+        
+        self.value = ""
+
+    def __str__(self):
+        return self.observer_prompt + " " + self.value
+
+    def set_value(self, value):
+        self.value = value
+    
